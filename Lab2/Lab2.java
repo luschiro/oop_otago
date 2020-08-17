c1635b0f7aed4ad35a5b71c65b0312be0c424b09
Lurvetti <lurvetti@gmail.com>
2020-08-17 18:21:52 -0300

    Lab 2 start

---
+++ Lab2/Lab2.java
@@ -0,0 +1,14 @@
+public class Lab2{
+    public static void main(final String[] args) {
+        System.out.println("50 plus 25 is " + 50 + 25);
+
+        int i = 7;
+        int x = 3;
+        x = i;
+        i = x;
+
+        System.out.println("x = " + x);
+        System.out.println("i = " + i);
+
+    }
+}
