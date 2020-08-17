9c1908c1e74a2e972cd83c348f785a8f1b1b9cc8
Lurvetti <lurvetti@gmail.com>
2020-08-17 20:40:46 -0300

    clear

---
+++ Lab2/TwoNumbersApp.java
@@ -0,0 +1,16 @@
+/**  Lab 2 COMP160 Part 1 TwoNumbersApp.java    * (your name) July 2014 */
+public class TwoNumbersApp{ 
+    public static <String> void main(String[] args) {
+        double num1;  //declaration, specifies data type and name
+        double num2;
+        double sum;
+        
+        num1 = 8.5;  //assignment
+        num2 = 15.0;
+        sum = num1 + num2;
+
+        System.out.println("First number is " + num1); 
+        System.out.println("Second number is " + num2); 
+        System.out.println("Sum is " +  sum);
+    }
+} 
